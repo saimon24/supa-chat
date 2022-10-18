1a0d1faf939b10109adde78266d51fd1c36ad309
Simon Grimm <saimon@devdactic.com>
2022-10-18 10:13:54 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'supaAuth',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
